35bb76e5357f1229a84df97343efea3e29c019b9
Ben <80292993+Benjino16@users.noreply.github.com>
2026-05-13 00:26:10 +0200

    fix: update url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://docs.batch4llm.de',
 	integrations: [
 		starlight({
 			title: 'Batch4LLM docs',
